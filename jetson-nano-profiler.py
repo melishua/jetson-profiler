f168374636b0aaddcdbb74d3a54272c28728dbd4
Melishua <panmelissa@hotmail.com>
2024-04-29 14:25:05 -0400

    try nanollm model stats API

--- jetson-nano-profiler.py
+++ jetson-nano-profiler.py
@@ -1,9 +1,9 @@
-from nano_llm import NanoLLM
+from nano_llm import NanoLLM, bot_function
 import os
 import json
 import argparse
 import random
-
+from datetime import datetime
 
 #################### CONSTANTS ####################
 def parse_arguments():
@@ -18,6 +18,16 @@ def parse_arguments():
     return parser.parse_args()
 
 ###################### UTILS ######################
+@bot_function
+def DATE():
+    """ Returns the current date. """
+    return datetime.now().strftime("%A, %B %-m %Y")
+   
+@bot_function
+def TIME():
+    """ Returns the current time. """
+    return datetime.now().strftime("%-I:%M %p")
+
 def process_shareGPT_json(file_path):
     cache_path = file_path.replace('.json', '.cache')
     
@@ -82,14 +92,24 @@ def main():
     # Run the prompt experiment for the specified number of iterations
     try:
         for i in range(args.num_iterations):
+            # Data collection
+            print(f"{TIME()}: Starting iteration {i}")
+            num_input_tokens = 0
+            num_output_tokens = 0
+            
             for p in prompts:
                 response = model.generate(p, max_new_tokens=args.max_new_tokens)
                 for token in response:
                     print(token, end='', flush=True)
+            print(f"model stats= {model.stats()}")
+            # Stats for this prompt set
+            # print(f"Number of input tokens: {num_input_tokens}")
+            # print(f"Number of output tokens: {num_output_tokens}")        
+            
     except Exception as e:
         print(f"Exception: {e}")
-        print(f"Currently at iteration {i}/{args.num_iterations}.")
-        print(f"Stopping the experiment now, bye!")
+        print(f"Stopped at iteration {i}/{args.num_iterations}.")
+        print(f"Exiting the experiment, bye!")
 
     os.system(f'touch {args.end_signal}')
     cleanup_files(args.start_signal)
